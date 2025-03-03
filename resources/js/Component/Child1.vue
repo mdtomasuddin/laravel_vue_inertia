3b16bebb87e2186d5dc4b9d9e658234afc41f9e4
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-03-03 08:47:00 +0600

    passing data controller to vue page to

---
+++ resources/js/Component/Child1.vue
@@ -0,0 +1,16 @@
+<script setup>
+  const props= defineProps({data: Object});
+</script>
+ 
+<template>
+<h4>passing data controller to page </h4>
+<h4>name : {{ props.data.name }}</h4>
+<h4>age : {{ props.data.age }}</h4>
+<h4>city : {{ props.data.city }}</h4>
+
+
+</template>
+ 
+<style scoped>
+ 
+</style>
--- resources/js/Pages/page1.vue
+++ resources/js/Pages/page1.vue
@@ -1,16 +1,20 @@
 <script setup>
  import { data } from 'autoprefixer';
 import AppNavBar from '../Component/AppNavBar.vue'
+import Child1 from '../Component/Child1.vue';
 
  const props= defineProps({data: Object});
 </script>
  
 <template>
 <AppNavBar />
+<h1>passing data controller to page </h1>
 <h1>name : {{ props.data.name }}</h1>
 <h1>age : {{ props.data.age }}</h1>
 <h1>city : {{ props.data.city }}</h1>
-<h1>Page 1  </h1>
+
+
+<Child1 :data="props.data" />
 
 </template>
  
